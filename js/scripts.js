121945a54e60f606c92ff0e482587fc7a94b5e7a
JadyVella <felixouma379@gmail.com>
2019-11-30 19:48:40 +0300

    Added a README.md file, css folder with bootsrap file inside it, index.css, index.html and JavaScript

---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+function validate() {
+    var dateOfBirth = document.getElementById("birth");
+    var peronality = document.getElementById("sex");
+    var yearsOfBirth = document.getElementById("age");
+};
+
+var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwatu", "Yaw", "Kofi", "Kwame"];
+var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var DaysOfTheW
+var DaysOfTheWeek = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD) mod7;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*if (DaysOfTheWeek == "1") {
+    console.log("You were borned on the 1st day of the week");
+}
+else if (DaysOfTheWeek == "2") {
+    console.log("You were borned on the 2nd day of the week");
+}
+else if (DaysOfTheWeek == "3") {
+    console.log("You were borned on the 3rd day of the week");
+}
+else if (DaysOfTheWeek == "4") {
+    console.log("You were borned on the 4th day of the week");
+}
+else if (DaysOfTheWeek == "5") {
+    console.log("You were borned on the 5th day of the week");
+}
+else if (DaysOfTheWeek == "6") {
+    console.log("You were borned on the 6th day of the week");
+}
+else if (DaysOfTheWeek == "7") {
+    console.log("You were borned on the 7th day of the week");
+}*/
