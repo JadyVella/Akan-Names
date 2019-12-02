a1246e3ecaa6fd15330f198ac092b98771b6fc02
JadyVella <felixouma379@gmail.com>
2019-12-02 09:46:59 +0300

    Added another input called

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,53 @@
 function validate() {
-    var dateOfBirth = document.getElementById ("birth");
-    var peronality = document.getElementById("sex");
-    var yearsOfBirth = document.getElementById("age");
-};
+    var birthDate = document.getElementById("birth");
+    // var maleFemale = document.getElementById("sex");
+    var birthYear = document.getElementById("age");
+ 
+var CC = parseInt(birthDate.slice(0,2));
+var YY = parseInt(birthYear.slice(2));
+var MM = parseInt();
+var dd = parseInt("birthDate");
+var sex = prompt("Enter sex");
+//var males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+//var females = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
+var day =  Math.ceil( ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + dd )%7);
 
-var dD = parseInt(dateOfBirth);
-var gender = "male, female";
-var daysOfTheWeek = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+if (sex = "male"  && day == 0) {
+    alert("You were born on Sunday and your Akan name is kwasi");
 
-
-
-if (gender == "male"  && dD == 0) {
-    alert ("You were born on Sunday and your Akan name is Kwasi");
+}
+else if (gender = "female" && day == 0) {
+    alert("You were born on Sunday and your Akan name is Akosua");
+}else if (gender = "male" && day == 1) {
+    alert("You were born on Monday and your Akan name is Kwadwo");
+}else if (gender = "female" && day == 1) {
+    alert("You were born on Monday and your Akan name is Adwoa");
+}else if (gender = "male" && day == 2) {
+    alert("You were born on Tuesday and your Akan name is Kwabena");
+}else if (gender = "female" && day == 2) {
+    alert("You were born on Tuesday and your Akan name is Abenaa");
+}else if (gender = "male" && day == 3) {
+    alert("You were born on Wednesday and your Akan name is Kwaku");
+}else if (gender = "female" && day == 3) {
+    alert("You were born on Wednesday and your Akan name is Akua");
+}else if (gender = "male" && day == 4) {
+    alert("You were born on Thursday and your Akan name is Yaw");
+}else if (gender = "female" && day == 4) {
+    alert("You were born on Thursday and your Akan name is Yaa");
+}else if (gender = "male" && day == 5) {
+    alert("You were born on Friday and your Akan name is Kofi");
+}else if (gender = "female" && day == 5) {
+    alert("You were born on Friday and your Akan name is Afua");
+}else if (gender = "male" && day == 6) {
+    alert("You were born on Saturday and your Akan name is Kwame");
+}else if (gender = "female" && day == 6) {
+    alert("You were born on Saturday and your Akan name is Ama");
 }
 
-else if (gender == "female" && dD == 0) {
-    alert ("you were born on Sunday and your Akan name is Akosua");
 }
 
-else if (gender == "male" && dD == 1) {
-    alert ("you were born on Monday and your Akan name is Kwadwo");
-}
 
-else if (gender == "female" && dD == 1) {
-    alert ("you were born on Monday and your Akan name is Adwoa");
-}
 
 
 
